3ea088906ae417809d7e6e9ad26cec651bc6a3d9
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2022-10-25 18:59:52 +0300

    Add more set/get functions

--- zr10_python.py
+++ zr10_python.py
@@ -170,6 +170,10 @@ class ZR10SDK:
         else:
             return ''
 
+    ###############################################################################
+    #                                Message construction                         #
+    ###############################################################################
+
     def acquireFirmwareVersionMsg(self) -> str:
         """
         Prepares the Acquire Firmware Version message, and returns it as a string
@@ -210,7 +214,7 @@ class ZR10SDK:
         cmd_id = "04"
         return self.encodeMsg(data_len, data, cmd_id)
 
-    def stopZoomMsg(self, flag) -> str:
+    def stopZoomMsg(self) -> str:
         """
         Prepares the Stop Zoom message, and returns it as a string
 
@@ -312,7 +316,7 @@ class ZR10SDK:
         cmd_id = "0a"
         return self.encodeMsg(data_len, data, cmd_id)
 
-    def functionInfoMsg(self) -> str:
+    def functionFeedbackMsg(self) -> str:
         """
         Prepares the Function Feedback Information message, and returns it as a string
 
@@ -403,6 +407,9 @@ class ZR10SDK:
         cmd_id = "0d"
         return self.encodeMsg(data_len, data, cmd_id)
     
+    ###############################################################################
+    #                                    Get  functions                           #
+    ###############################################################################
     def getFirmwareVersion(self):
         """
         Sends msg to request firmware version
@@ -414,7 +421,7 @@ class ZR10SDK:
             # Get feedback, timesout after 1 second
             server_msg = self.rcvMsg()
             if server_msg is None:
-                print("[getFirmwareVersion] Did not get feedback from server within 1 second")
+                print("[getFirmwareVersion] Did not get feedback from camera within 1 second")
                 return None
 
             # TODO decode msg
@@ -440,7 +447,7 @@ class ZR10SDK:
             # Get feedback, timesout after 1 second
             server_msg = self.rcvMsg()
             if server_msg is None:
-                print("[getHardwareID] Did not get feedback from server within 1 second")
+                print("[getHardwareID] Did not get feedback from camera within 1 second")
                 return None
 
             # TODO decode msg
@@ -471,7 +478,6 @@ class ZR10SDK:
         roll_velocity: [float] roll speed in degrees/second
         """
         msg = self.gimbalAttitudeMsg()
-        print("[getGimbalAttitude] attitude msg: ", msg)
         if len(msg)>0:
             self.sendMsg(msg)
             # Get feedback, timesout after 1 second
@@ -500,7 +506,194 @@ class ZR10SDK:
             print("[getGimbalAttitude] Could not construct msg")
             return None
 
-   
+    def getGimbalInfo(self):
+        """
+        Sends msg to request gimbal configuration information
+
+        Returns
+        --
+        - hdr_state: [int]HDR status 1: ON, 0: OFF
+        - record_state: [int] Recording status 0:OFF, 1: ON, 2: TF card slot is empty, 3:(Recording) Data loss in TF card recorded video, please check TF card
+        - motion_mode: [int] Gimbal Motion mode 0: lock mode, 1: follow mode, 2: FPV mode
+        - mounting_dir: [int] Mounting direction 1: Normal, 2: Upside down
+        """
+        msg = self.gimbalInfoMsg()
+        if len(msg)>0:
+            self.sendMsg(msg)
+            # Get feedback, timesout after 1 second
+            server_msg = self.rcvMsg()
+            if server_msg is None:
+                print("[getGimbalInfo] Did not get feedback from camera within 1 second")
+                return None
+
+            print("[getGimbalInfo] Server msg: ", server_msg.hex())
+
+            # decode msg
+            hex_str = self.decodeMsg(server_msg.hex())
+            print("[getGimbalInfo] Data hex sting: ", hex_str)
+            if len(hex_str)==0:
+                return None
+
+            hdr_state = int(hex_str[2:4], base=16)
+            record_state = int(hex_str[6:8], base=16)
+            motion_mode = int(hex_str[8:10], base=16)
+            mount_dir = int(hex_str[10:12], base=16)
+            return hdr_state, record_state, motion_mode, mount_dir
+                
+        else:
+            print("[getGimbalInfo] Could not construct msg")
+            return None
+
+    def getFuncFeedback(self):
+        """
+        Sends msg to request Function Feedback Information
+
+        Returns
+        --
+        ack_data: [int]
+                    0: success, 1: Fail to take a photo (Please check if TF card is inserted)
+                    2: HDR ON, 3: HDR OFF
+                    4: Fail to record a video (Please check if TF card is inserted)
+        """
+        msg = self.functionFeedbackMsg()
+        if len(msg)>0:
+            self.sendMsg(msg)
+            # Get feedback, timesout after 1 second
+            server_msg = self.rcvMsg()
+            if server_msg is None:
+                print("[getFuncFeedback] Did not get feedback from camera within 1 second")
+                return None
+
+            # print("[getFuncFeedback] Server msg: ", server_msg.hex())
+
+            # decode msg
+            hex_str = self.decodeMsg(server_msg.hex())
+            # print("[getFuncFeedback] Data hex sting: ", hex_str)
+            if len(hex_str)==0:
+                return None
+
+            ack_data = int(hex_str, base=16)
+            return ack_data
+                
+        else:
+            print("[getFuncFeedback] Could not construct msg")
+            return None
+
+    ###############################################################################
+    #                                    Set  functions                           #
+    ###############################################################################
+    def setAutoFocus(self):
+        """
+        Sends msg to request auto focus.
+
+        Reutrns
+        --
+        True: Success
+        False: Fail
+        """
+        msg = self.autoFocusMsg()
+        print("[setAutoFocus] autofocus hex string: ", msg)
+        if len(msg)>0:
+            self.sendMsg(msg)
+            # Get feedback, timesout after 1 second
+            server_msg = self.rcvMsg()
+            print("[setAutoFocus] server_msg hex string: ", server_msg.hex())
+            if server_msg is None:
+                print("[setAutoFocus] Did not get feedback from camera within 1 second")
+                return None
+            
+            # Decode mesage
+            hex_str = self.decodeMsg(server_msg.hex())
+            print("[setAutoFocus] Data hex sting: ", hex_str)
+            if len(hex_str)==0:
+                return None
+
+            flag = int(hex_str, base=16)
+            if flag==1:
+                return True
+            else:
+                return False
+                
+        else:
+            print("[setAutoFocus] Could not construct msg")
+            return None
+
+    def setZoom(self, flag):
+        """
+        Sends zoom request
+
+        Params
+        --
+        flag: [int] 1: start zoom in, 0: stop zoom, -1: start zoom out
+        
+        Returns
+        --
+        zoom_level: [int] 0~30
+        """
+        if (flag == 1):
+            msg=self.zoomInMsg()
+        elif (flag == -1):
+            msg=self.zoomOutMsg()
+        else:
+            msg=self.stopZoomMsg()
+
+        if len(msg)>0:
+            self.sendMsg(msg)
+            # Get feedback, timesout after 1 second
+            server_msg = self.rcvMsg()
+            if server_msg is None:
+                print("[setZoom] Did not get feedback from camera within 1 second")
+                return None
+            
+            # Decode mesage
+            hex_str = self.decodeMsg(server_msg.hex())
+            print("[setZoom] Data hex sting: ", hex_str)
+            if len(hex_str)==0:
+                return None
+
+            zoom_level = int(hex_str[2:4]+hex_str[0:2], base=16) /10.
+            return zoom_level
+                
+        else:
+            print("[setZoom] Could not construct msg")
+            return None
+
+    def setFocus(self, flag):
+        """
+        Sends manual focus request
+
+        Params
+        --
+        flag: [int] 1: Long shot, 0: stop manual focus, -1: close shot
+        
+        Returns
+        --
+        True: Success
+        False: Fail
+        """
+        msg=self.manualFocusMsg(flag)
+        if len(msg)>0:
+            self.sendMsg(msg)
+            # Get feedback, timesout after 1 second
+            server_msg = self.rcvMsg()
+            if server_msg is None:
+                print("[setFocus] Did not get feedback from camera within 1 second")
+                return None
+            
+            # Decode mesage
+            hex_str = self.decodeMsg(server_msg.hex())
+            # print("[setFocus] Data hex sting: ", hex_str)
+            if len(hex_str)==0:
+                return None
+
+            ret = int(hex_str, base=16)
+            
+            return bool(ret)
+                
+        else:
+            print("[setFocus] Could not construct msg")
+            return None
+
     def centerGimbal(self):
         """
         Sends msg to set gimbal at the center position
@@ -520,7 +713,7 @@ class ZR10SDK:
 
             # TODO decode msg
             hex_str = self.decodeMsg(server_msg.hex())
-            print("[centerGimbal] Data hex sting: ", hex_str)
+            # print("[centerGimbal] Data hex sting: ", hex_str)
             if len(hex_str)==0:
                 return None
 
@@ -552,12 +745,12 @@ class ZR10SDK:
             # Get feedback, timesout after 1 second
             server_msg = self.rcvMsg()
             if server_msg is None:
-                print("[setGimbalSpeed] Did not get feedback from server within 1 second")
+                print("[setGimbalSpeed] Did not get feedback from camera within 1 second")
                 return None
 
             # TODO decode msg
             hex_str = self.decodeMsg(server_msg.hex())
-            print("[setGimbalSpeed] Data hex sting: ", hex_str)
+            # print("[setGimbalSpeed] Data hex sting: ", hex_str)
             if len(hex_str)==0:
                 return None
 
@@ -570,7 +763,7 @@ class ZR10SDK:
             print("[setGimbalSpeed] Could not construct msg")
             return None
 
-    def setGimbalAttitude(self, yaw, pitch, err_thresh=1):
+    def setGimbalRotation(self, yaw, pitch, err_thresh=1):
         """
         Sets gimbal attitude angles yaw and pitch in degrees
 
@@ -581,11 +774,11 @@ class ZR10SDK:
         err_thresh: [float] acceptable error threshold, in degrees, to stop correction
         """
         if (pitch >25 or pitch <-90):
-            print("[setGimbalAttitude] ERROR. desired pitch is outside controllable range -90~25")
+            print("[setGimbalRotation] ERROR. desired pitch is outside controllable range -90~25")
             return
 
         if (yaw >45 or yaw <-45):
-            print("[setGimbalAttitude] ERROR. desired yaw is outside controllable range -45~45")
+            print("[setGimbalRotation] ERROR. desired yaw is outside controllable range -45~45")
             return
 
         # TODO 
@@ -594,7 +787,7 @@ class ZR10SDK:
         while(True):
             vals = self.getGimbalAttitude()
             if vals is None:
-                print("[setGimbalAttitude] Error. Gimbal attitude is None")
+                print("[setGimbalRotation] Error. Gimbal attitude is None")
                 break
 
             y,p,r, y_s, p_s, r_s = vals[0], vals[1], vals[2], vals[3], vals[4], vals[5]
@@ -606,17 +799,158 @@ class ZR10SDK:
 
             if (abs(yaw_err) <= th and abs(pitch_err)<=th):
                 ret = self.setGimbalSpeed(0, 0)
-                print("[setGimbalAttitude] Goal reached")
+                print("[setGimbalRotation] Goal reached")
                 break
             y_speed_sp = max(min(100, int(gain*yaw_err)), -100)
             p_speed_sp = max(min(100, int(gain*pitch_err)), -100)
-            print("[setGimbalAttitude] yaw speed setpoint= ", y_speed_sp)
-            print("[setGimbalAttitude] pitch speed setpoint= ", p_speed_sp)
+            print("[setGimbalRotation] yaw speed setpoint= ", y_speed_sp)
+            print("[setGimbalRotation] pitch speed setpoint= ", p_speed_sp)
             ret = self.setGimbalSpeed(y_speed_sp, p_speed_sp)
             if(not ret):
-                print("[setGimbalAttitude] Could not set gimbal speed")
+                print("[setGimbalRotation] Could not set gimbal speed")
                 break
             sleep(0.1)
+
+    def takePhoto(self):
+        """
+        Sends a message to take a single photo
+
+        Returns
+        --
+        True: if success. False otherwise
+        """
+        msg = self.takePhotoMsg()
+        if len(msg)>0:
+            self.sendMsg(msg)
+        # sleep(0.1) # Let the command sink
+
+        ack_code = self.getFuncFeedback()
+
+        if ack_code is None:
+            print("[takePhoto] Could not get acknowledgement")
+            return False
+
+        if ack_code==0:
+            return True
+        else:
+            print("[takePhoto] Could not take photo. Error code: ", ack_code)
+            return False
+
+    def toggleRecording(self):
+        """
+        Sends a message to toggle recording state. 
+        
+        Returns
+        --
+        True: if success. False otherwise
+        """
+
+        msg = self.startStopRecordingMsg()
+        if len(msg)>0:
+            self.sendMsg(msg)
+        sleep(0.1) # Let the command sink
+
+        ack_code = self.getFuncFeedback()
+        if ack_code is None:
+            print("[takePhoto] Could not get acknowledgement")
+            return False
+
+        if ack_code > 0:
+            print("[toggleRecording] Could not toggle recording. Function Feedback Error code: ", ack_code)
+            return False
+
+        info=self.getGimbalInfo()
+        if info is None:
+            print("[toggleRecording] Could not get gimbal info for acknowledgement")
+            return False
+
+        hdr_state, record_state, motion_mode, mount_dir = info[0], info[1], info[2], info[3]
+
+        
+        if record_state == 1:
+            print("[toggleRecording] Recording is ON")
+            return True
+        elif record_state == 0:
+            print("[toggleRecording] Recording is OFF")
+            return True
+        else:
+            print("[toggleRecording] Record state is unknown . Code: ", record_state)
+            return False
+
+    def setLockMode(self):
+        """
+        Sends a message to set Lock mode
+
+        Returns
+        --
+        True: if success. False otherwise
+        """
+        msg = self.lockModeMsg()
+        print("[setLockMode] Lock mode msg", msg)
+        if len(msg)>0:
+            self.sendMsg(msg)
+        sleep(0.1) # Let the command sink
+
+        ack_code = self.getFuncFeedback()
+
+        if ack_code is None:
+            print("[setLockMode] Could not get acknowledgement")
+            return False
+
+        if ack_code==0:
+            return True
+        else:
+            return False
+
+    def setFollowMode(self):
+        """
+        Sends a message to set Follow mode
+
+        Returns
+        --
+        True: if success. False otherwise
+        """
+        msg = self.followModeMsg()
+        if len(msg)>0:
+            self.sendMsg(msg)
+        sleep(0.1) # Let the command sink
+
+        ack_code = self.getFuncFeedback()
+
+        if ack_code is None:
+            print("[setFollowMode] Could not get acknowledgement")
+            return False
+
+        if ack_code==0:
+            return True
+        else:
+            return False
+
+    def setFPVMode(self):
+        """
+        Sends a message to set FPV mode
+
+        Returns
+        --
+        True: if success. False otherwise
+        """
+        msg = self.fpvModeMsg()
+        if len(msg)>0:
+            self.sendMsg(msg)
+        sleep(0.1) # Let the command sink
+
+        ack_code = self.getFuncFeedback()
+
+        if ack_code is None:
+            print("[setFPVMode] Could not get acknowledgement")
+            return False
+
+        if ack_code==0:
+            return True
+        else:
+            return False
+            
+
 
 
 def test():
@@ -636,13 +970,56 @@ def test():
     cam.centerGimbal()
     sleep(2)
 
-    cam.setGimbalAttitude(0,-80)
+    # cam.setGimbalRotation(0,-80)
 
-    y,p,r,y_speed, p_speed, r_speed = cam.getGimbalAttitude()
-    print("Attitude [deg] yaw, pitch, roll: ", y,p,r)
-    print("Attitude speed [deg/s]: ", y_speed,p_speed,r_speed)
+    # y,p,r,y_speed, p_speed, r_speed = cam.getGimbalAttitude()
+    # print("Attitude [deg] yaw, pitch, roll: ", y,p,r)
+    # print("Attitude speed [deg/s]: ", y_speed,p_speed,r_speed)
 
-   
+    # hdr_state, record_state, motion_mode, mount_dir=cam.getGimbalInfo()
+    # print("HDR state: ", hdr_state)
+    # print("Recording state: ", record_state)
+    # print("Motion mode: ", motion_mode)
+    # print("Mounting Direction: ", mount_dir)
+
+    # fb = cam.setFocus(1)
+    # print("[test] Manual focus, long shot: ", fb)
+    # sleep(1)
+    # fb = cam.setFocus(0)
+    # print("[test] Manual focus, close shot: ", fb)
+    # sleep (1)
+
+    # fb = cam.setAutoFocus()
+    # print("[test] Set auto focus: ", fb)
+
+    
+    # val = cam.setZoom(-1)
+    # print("[test] Zooming out, level: ", val)
+    # sleep(5)
+    # val = cam.setZoom(0)
+    # print("[test] Stop zoom, level: ", val)
+
+    # val = cam.setZoom(1)
+    # print("[test] Zooming in, level: ", val)
+    # sleep(1)
+    
+    # val = cam.setZoom(0)
+    # print("[test] Stop zoom, level: ", val)
+
+    # fb = cam.takePhoto()
+    # print("[test] Take photo: ", fb)
+
+    # fb = cam.toggleRecording()
+    # print("[test] Toggle recording: ", fb)
+
+    # fb = cam.setFPVMode()
+    # print("[test] Set FPV mode: ", fb)
+
+    # fb = cam.setFollowMode()
+    # print("[test] Set Follow mode: ", fb)
+
+    # fb = cam.setLockMode()
+    # print("[test] Set Lock mode: ", fb)
     
 
 if __name__ == "__main__":
