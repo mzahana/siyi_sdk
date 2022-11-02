3c5cfb4c69b72270d1525ed84cf176740e5cad1b
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2022-11-02 11:58:32 +0300

    Add tests/test_zoom.py

---
+++ tests/test_zoom.py
@@ -0,0 +1,42 @@
+"""
+@file test_center_gimbal.py
+@Description: This is a test script for using the SIYI SDK Python implementation to adjust zoom level
+@Author: Mohamed Abdelkader
+@Contact: mohamedashraf123@gmail.com
+All rights reserved 2022
+"""
+
+import sys
+import os
+from time import sleep
+  
+current = os.path.dirname(os.path.realpath(__file__))
+parent_directory = os.path.dirname(current)
+  
+sys.path.append(parent_directory)
+
+from siyi_sdk import SIYISDK
+
+def test():
+    cam = SIYISDK(server_ip="192.168.144.25", port=37260)
+
+    if not cam.connect():
+        print("No connection ")
+        exit(1)
+
+    val = cam.requestZoomIn()
+    sleep(1)
+    val = cam.requestZoomHold()
+    sleep(1)
+    print("Zoom level: ", cam.getZoomLevel())
+
+    val = cam.requestZoomOut()
+    sleep(1)
+    val = cam.requestZoomHold()
+    sleep(1)
+    print("Zoom level: ", cam.getZoomLevel())
+
+    cam.disconnect()
+
+if __name__ == "__main__":
+    test()
