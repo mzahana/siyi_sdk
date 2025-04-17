f8cad14ca3138eca2438d93ba2f9a30c4591a040
ob-bb <robert@projecteagle.net>
2025-04-17 15:21:00 -0700

    added camera ZT6 and fixed some other issues

--- cameras.py
+++ cameras.py
@@ -15,3 +15,10 @@ class ZR10:
     MAX_PITCH_DEG = 25.0
     MIN_PITCH_DEG = -90.0
     MAX_ZOOM = 30.0 # 10 optical * 3 digital
+
+class ZT6:
+    MAX_YAW_DEG = 270.0
+    MIN_YAW_DEG = -270.0
+    MAX_PITCH_DEG = 25.0
+    MIN_PITCH_DEG = -90.0
+    MAX_ZOOM = 6.0 # 10 optical * 3 digital
